6d3cef6582068595715bc0b522baa1b80152fc83
Victoriaspurlieu <burberry0812sh@gmail.com>
2019-07-02 22:05:28 -0700

    updated sql files

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Mountain Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Volcano-Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Aloha-Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("DoubleDouble");
+
+INSERT INTO burgers (burger_name) VALUES ("All Vegan Burger");
